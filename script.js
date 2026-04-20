1fa51990cb456576c1a11b4c98e872cdcc69b300
induranga <indurangadilshan2003@gmail.com>
2026-04-20 19:31:05 +0530

    Update CSS and JavaScript for responsive design and header behavior

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const MIN_STICKER_SIZE = 24;
 const MAX_STICKER_SIZE = 56;
 const MAX_CUSTOM_ITEM_QUANTITY = 20;
 let lastMeasuredHeaderHeight = 0;
+let maxMeasuredHeaderHeight = 0;
 
 const getSavedBranchName = () => {
   try {
@@ -76,13 +77,15 @@ const syncStickyHeaderOffset = () => {
   }
 
   const headerHeight = Math.round(pageHeader.getBoundingClientRect().height);
-  if (headerHeight === lastMeasuredHeaderHeight) {
-    return;
+  if (headerHeight !== lastMeasuredHeaderHeight) {
+    lastMeasuredHeaderHeight = headerHeight;
+    document.documentElement.style.setProperty("--side-menu-top", `${headerHeight}px`);
   }
 
-  lastMeasuredHeaderHeight = headerHeight;
-  document.documentElement.style.setProperty("--side-menu-top", `${headerHeight}px`);
-  document.documentElement.style.setProperty("--sticky-header-height", `${headerHeight}px`);
+  if (headerHeight > maxMeasuredHeaderHeight) {
+    maxMeasuredHeaderHeight = headerHeight;
+    document.documentElement.style.setProperty("--sticky-header-height", `${maxMeasuredHeaderHeight}px`);
+  }
 };
 
 const initializeCollapsibleHeader = () => {
